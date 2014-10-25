aeaf2556df18e747977d54d615ea5a1dd6ef3381
Cody Casterline <cody@wpengine.com>
2014-10-25 16:59:03 +0000

    Clean up the logic.

--- mb.py
+++ mb.py
@@ -1,55 +1,46 @@
 #!/usr/bin/env python3
 
 from random import randint
+import unittest
 
 def main(args):
-	print("Mapper count:", mapper.count)
-
 	while True:
 		i = input()
 		if not i: break
 		show(i)
 
 def show(m):
-	print(m)
-	print(bytes_to_mojibake(m))
-
-
-
-
-def bytes_to_mojibake(b):
-	if type(b) is str:
-		b = b.encode()
-
-	out = ""
-	while b:
-		(head, b) = (b[:2], b[2:])
-		if len(head) == 1:
-			out += "="
-			head = bytes([0]) + head
-		l = (head[0] << 8) | head[1]
-		out += chr(mapper.map_into(l))
-
-	return out
-
-
-
-def chunk(seq, size=2):
-	if len(seq) == 0:
-		yield []
-		return
-	# else:
-	while seq:
-		(head, seq) = (seq[:size], seq[size:])
-		yield head
+	print(mapper.bytes_to_mojibake(m))
 	
 
+class MultiRange:
+	"""A range-like object that joins a multiple disjoint ranges."""
+
+	def __init__(self, ranges):
+		self.__ranges = ranges
+
+	def __len__(self):
+		return sum([len(r) for r in self.__ranges])
+
+	def index(self, item):
+		offset = 0
+		for r in self.__ranges:
+			if item in r: return offset + r.index(item)
+			# else:
+			offset += len(r)
+		raise ValueError("{} is not in range".format(item))
+
+	def __getitem__(self, i):
+		for r in self.__ranges:
+			if i < len(r): return r[i]
+			else: i -= len(r)
+		raise IndexError("range object index ({}) out of range".format(i));
 
 	
 class Mapper:
 	"""Maps a number into a set of ranges of numbers."""
 
-	ranges = [
+	characters = MultiRange([
 		# http://en.wikipedia.org/wiki/Unicode_block
 		# CJK Unified Ideographs Extension B
 		range(0x20000, 0x2A6E0),
@@ -57,31 +48,41 @@ class Mapper:
 		range(0x4E00, 0xA000),
 		# Hangul Syllables
 		range(0xAC00, 0xD7B0),
-	]
+	])
 
-	@property 
-	def count(self):
-		count = 0
-		for r in self.ranges:
-			print(len(r))
-			count += len(r)
-		return count
+	# For odd numbers of bytes, the last (single) byte will
+	# be encoded in the 256 values above 2^16:
+	single_byte_offset = 0x10000
 
-	def map_into(self, num):
-		for r in self.ranges: 
-			if num < len(r): return r[num]
-			else: num -= len(r)
-		raise Exception("Can't map_into({}}".format(num))
+	def bytes_to_mojibake(self, b):
+		if type(b) is str:
+			b = b.encode()
 
-	def map_outof(self, num):
-		offset = 0
-		for r in self.ranges:
-			if num in r: return offset + (num - r.start)
-			else: offset += len(r)
-		raise Exception("Can't map_outof({}}".format(num))
+		out = ""
+		while b:
+			(head, b) = (b[:2], b[2:])
+			if len(head) == 2: offset = (head[0] << 8) | head[1]
+			else: offset = self.single_byte_offset + head[0] 
+			out += chr(self.characters[offset])
+
+		return out
+
 
 mapper = Mapper()
 
+
+
+class MojibakeTests(unittest.TestCase):
+
+	def test_enough_bytes(self):
+		# We need at least 2^16 code points for 2-byte representations
+		# and an extra 2^8 for one-byte representations.
+		min_size = 0x10100
+
+		self.assertTrue(len(mapper.characters) >= min_size)
+
+
 if __name__ == "__main__":
 	import sys
+	#unittest.main()
 	main(sys.argv[1:])
